a6430bf57317dfb26b5e923ab6ed61d1fdb61027
AbdirahmanOmar-90~ <cukashaomar92@gmail.com>
2026-01-19 14:21:57 +0300

    add another console

--- Exercise_5/main.js
+++ Exercise_5/main.js
@@ -1,6 +1,7 @@
-let mult = (x,y) =>{
-        return x*y
+let mult = (x, y) => {
+  return x * y
 }
-console.log(mult(4,30));
-console.log(mult(4,10));
-console.log(mult(19,19));
+console.log(mult(4, 30))
+console.log(mult(4, 10))
+console.log(mult(19, 19))
+console.log(mult(100,10))
