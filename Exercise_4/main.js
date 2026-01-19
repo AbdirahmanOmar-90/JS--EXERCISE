dd470c7070d093317e8e922a98c7383da43f9196
AbdirahmanOmar-90~ <cukashaomar92@gmail.com>
2026-01-19 14:04:25 +0300

    submit exercise-4

---
+++ Exercise_4/main.js
@@ -0,0 +1,15 @@
+function add(v, b) {
+  return v + b
+}
+console.log(add(11, 44))
+console.log(add(40,50));
+console.log(add(99,88));
+console.log(add(499,4999));
+
+
+const sub = function(d,k){
+        return d-k
+}
+console.log(sub(49,49));
+console.log(sub(499,299));
+console.log(sub(200,1000));
