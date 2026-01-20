a03264c29194215865dffdfe1b61ab7046958a83
AbdirahmanOmar-90~ <cukashaomar92@gmail.com>
2026-01-20 19:37:07 +0300

    i make last console of all element

--- Exercise_8/main.js
+++ Exercise_8/main.js
@@ -10,5 +10,6 @@ console.log(car)
 console.log(car.make)
 console.log(car.model)
 console.log(car.year)
-console.log(car.price)
+console.log(car.price) 
+console.log('Garigan waxa lasameyay',car.make,'tii bisha 7aad','waa nuuca la dhaho',car.model, 'wuxuu soo baxay', car.year,'tii', 'qiima hiisana waa',car.price,'kun')
 
