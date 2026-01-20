b62344035dccefd711199d61c58c3874c6b0c0c1
AbdirahmanOmar-90~ <cukashaomar92@gmail.com>
2026-01-20 19:38:15 +0300

    seved my code

--- Exercise_8/main.js
+++ Exercise_8/main.js
@@ -1,15 +1,25 @@
 const car = {
-        make: 2014,
-        model: 'bmw',
-        year: 2015,
-        price: 20000,
-
+  make: 2014,
+  model: 'bmw',
+  year: 2015,
+  price: 20000,
 }
 
 console.log(car)
 console.log(car.make)
 console.log(car.model)
 console.log(car.year)
-console.log(car.price) 
-console.log('Garigan waxa lasameyay',car.make,'tii bisha 7aad','waa nuuca la dhaho',car.model, 'wuxuu soo baxay', car.year,'tii', 'qiima hiisana waa',car.price,'kun')
-
+console.log(car.price)
+console.log(
+  'Garigan waxa lasameyay',
+  car.make,
+  'tii bisha 7aad',
+  'waa nuuca la dhaho',
+  car.model,
+  'wuxuu soo baxay',
+  car.year,
+  'tii',
+  'qiima hiisana waa',
+  car.price,
+  'kun',
+)
