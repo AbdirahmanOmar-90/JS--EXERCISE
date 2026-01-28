f710d25ae2be94f142404f7ee011c05e457731a5
AbdirahmanOmar-90~ <cukashaomar92@gmail.com>
2026-01-28 17:45:51 +0300

    solveing exercise10

---
+++ Exercise_10/main.js
@@ -0,0 +1,17 @@
+let labrary = [
+  {
+    title: 'rich dad por dad',
+    author: 'kiosaki',
+    year: 2019,
+  },
+
+  {
+    title: 'zero to one',
+    author: 'ali',
+    year: 2020,
+  },
+]
+  console.log('kani waxa itemka koo waad ee labraryga',labrary[0])
+console.log('kan waa labrary koo baada ciwan kiisa waana:',labrary[0].title)
+  console.log('kani waxa itemka labaad ee labraryga', labrary[1])
+console.log('buugaan waxaa qoray qoraa:',labrary[1].author)
