0a09dc44a2c4523cb72668b2a2c9bd1aab59876f
AbdirahmanOmar-90~ <cukashaomar92@gmail.com>
2026-02-11 15:17:06 +0300

    i consoled list of array in for loob

--- Exercise_12/main.js
+++ Exercise_12/main.js
@@ -1,3 +1,16 @@
 // for loob Exercise--12
 
-let forutes = ['mango', 'banana', 'limon', 'apple', 'papaya', 'tomato', 'genger', 'watermelon', '']
+let forutes = [
+  'mango',
+  'banana',
+  'limon',
+  'apple',
+  'papaya',
+  'tomato',
+  'genger',
+  'watermelon',
+  'onian',
+]
+for (let y = 0; y < forutes.length; y++) {
+  console.log('This is ', forutes[y])
+}
