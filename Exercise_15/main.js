2b1a26db1fa557d9b4e6699fb2ce6b101c5d4c6b
AbdirahmanOmar-90~ <cukashaomar92@gmail.com>
2026-02-15 16:47:17 +0300

    i create list of object

--- Exercise_15/main.js
+++ Exercise_15/main.js
@@ -1,1 +1,7 @@
 // for in and for of exercise_15
+
+let people = [
+  { name: 'ali', age: 20, city: 'mugadishu', yearofbarth: 2005 },
+  { name: 'ali', age: 20, city: 'mugadishu', yearofbarth: 2005 },
+  { name: 'ali', age: 20, city: 'mugadishu', yearofbarth: 2005 },
+]
