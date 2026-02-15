8f89e2477b6ffc8229975dc61c0d4f74af7d61a2
AbdirahmanOmar-90~ <cukashaomar92@gmail.com>
2026-02-15 16:53:36 +0300

    i consoled list of objects

--- Exercise_15/main.js
+++ Exercise_15/main.js
@@ -1,7 +1,14 @@
 // for in and for of exercise_15
 
 let people = [
-  { name: 'ali', age: 20, city: 'mugadishu', yearofbarth: 2005 },
-  { name: 'ali', age: 20, city: 'mugadishu', yearofbarth: 2005 },
-  { name: 'ali', age: 20, city: 'mugadishu', yearofbarth: 2005 },
+  { name: 'ali', age: 10, city: 'mugadishu', yearofbarth: 2011 },
+  { name: 'abdi', age: 20, city: 'mugadishu', yearofbarth: 2005 },
+  { name: 'aisha', age: 30, city: 'mugadishu', yearofbarth: 1995 },
 ]
+console.log('propertes and value of each person')
+for (let person of people) {
+  for (let key in person) {
+    console.log(key, ':', person[key])
+  }
+  console.log('----')
+}
