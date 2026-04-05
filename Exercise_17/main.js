84633a2ba183530fbc7d04cd34875c1cc9f8929b
AbdirahmanOmar-90~ <cukashaomar92@gmail.com>
2026-04-05 11:48:18 +0300

    thard consoele

--- Exercise_17/main.js
+++ Exercise_17/main.js
@@ -1,6 +1,8 @@
-let temp = 11
+let temp = 17
 if (temp == 0) {
   console.log('this very cold')
-} else if(temp >= 0 && temp<= 15 ){
-        console.log('this is cold')
+} else if (temp >= 0 && temp <= 15) {
+  console.log('this is cold')
+} else if (temp >= 15 && temp <= 25) {
+  console.log('this warm')
 }
