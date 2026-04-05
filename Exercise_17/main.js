af4c8a65b0d9bd5a6f1359e580b93b3c60b97d0b
AbdirahmanOmar-90~ <cukashaomar92@gmail.com>
2026-04-05 13:00:35 +0300

    i create index.html

--- Exercise_17/main.js
+++ Exercise_17/main.js
@@ -2,9 +2,9 @@ let temp = 27
 if (temp == 0) {
   console.log('this very cold') // frist console
 } else if (temp >= 0 && temp <= 15) {
-  console.log('this is cold')  // secon console
+  console.log('this is cold') // secon console
 } else if (temp >= 15 && temp <= 25) {
   console.log('this warm') // thard conole
-} else{
-        console.log('thi is hot') //forth console
+} else {
+  console.log('thi is hot') //forth console
 }
