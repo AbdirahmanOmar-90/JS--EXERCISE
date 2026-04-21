8d5282881a81ddbb1890e74da26c71a9a034c122
AbdirahmanOmar-90~ <cukashaomar92@gmail.com>
2026-04-21 22:03:34 +0300

    second console

--- Exercise_25/main.js
+++ Exercise_25/main.js
@@ -1,6 +1,7 @@
 let numbers = [1, 2, 3, 4, 5, 6]
 let newNumbers = [...numbers, 7, 8, 9, 10]
 console.log(newNumbers)
-function sum(num){
-       
+function sum(...numbers) {
+  return numbers.reduce((total, num) => total + num, 0)
 }
+console.log(sum(2, 2, 2, 2))
