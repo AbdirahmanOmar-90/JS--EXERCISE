64be813e8157b55366b05c953040fd9334564541
AbdirahmanOmar-90~ <cukashaomar92@gmail.com>
2026-06-14 22:24:09 +0300

    make user virable

--- Exercise_26/main.js
+++ Exercise_26/main.js
@@ -1,6 +1,7 @@
-
 //  blokeng exercise
-function fetchUserDateSync(){
-        alert('riix kadib ayad heleys jawab')
-        return{age: 55, country: 'somalia'}
+function fetchUserDateSync() {
+  alert('riix kadib ayad heleys jawab')
+  return { age: 55, country: 'somalia' }
 }
+console.log('jabwabta Kobaad')
+let user = fetchUserDateSync()
