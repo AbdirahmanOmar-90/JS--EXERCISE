9972e9eb9a43e52904739307b9be91ec748dd54a
AbdirahmanOmar-90~ <cukashaomar92@gmail.com>
2026-06-17 14:01:41 +0300

    make if

--- Exercise_27/main.js
+++ Exercise_27/main.js
@@ -2,6 +2,9 @@ function fetchUserData() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       let person = true
+      if (person) {
+        resolve({ name: 'abdirahmana', age: 22 })
+      }
     })
   })
 }
