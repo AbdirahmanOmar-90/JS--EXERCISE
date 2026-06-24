fbb4d8fe20a85dc90305763897180e46baa6ad7b
AbdirahmanOmar-90~ <cukashaomar92@gmail.com>
2026-06-24 17:12:06 +0300

    make some thange

--- Exercise_29/main.js
+++ Exercise_29/main.js
@@ -1,6 +1,6 @@
 async function fetch() {
   console.log('start fetch data')
-  let response = await fetch('datajson')
+  let response = await fetch('data.json')
   const data = await response.json()
   console.log('response', data)
 }
