176755aa5512b3264b31331ffa77db13b5b8d356
AbdirahmanOmar-90~ <cukashaomar92@gmail.com>
2026-08-29 20:46:07 +0300

    second console

--- Exersice_30/main.js
+++ Exersice_30/main.js
@@ -4,5 +4,8 @@ function Oprate(a, b, callback) {
 function add(a, b) {
   return a + b
 }
-
+function sup(a, b) {
+  return a - b
+}
 console.log('iskudarku waa', Oprate(44, 44, add))
+console.log('kala jarku waa', Oprate(30, 20, sup))
