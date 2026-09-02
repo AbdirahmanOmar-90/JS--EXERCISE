0f35218a787eabd88127262fbfe0170574245f54
AbdirahmanOmar-90~ <cukashaomar92@gmail.com>
2026-09-02 14:26:04 +0300

    last console

--- Exercise_31/main.js
+++ Exercise_31/main.js
@@ -2,6 +2,11 @@ async function getData() {
   try {
     const response = await fetch('https://jsonplaceholder.typicode.com/users')
     if (!response.ok) {
+      throw new Error(`http error status,${response.status}`)
     }
+    console.log('rospnse before json', response)
+    const data = await response.json()
+    console.log('response after json', data)
   } catch (error) {}
 }
+getData()
