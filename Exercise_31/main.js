063bd29f0991013f5b2ffa511f238f8d330f610e
AbdirahmanOmar-90~ <cukashaomar92@gmail.com>
2026-09-02 14:18:30 +0300

    change

--- Exercise_31/main.js
+++ Exercise_31/main.js
@@ -1,5 +1,5 @@
 async function getData() {
-  const response = await fetch('https://jsonplaceholder.typicode.com/users')
   try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
   } catch (error) {}
 }
