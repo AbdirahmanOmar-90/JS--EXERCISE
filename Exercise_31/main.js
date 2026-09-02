96d167804f6f1d9ac266a8e6e7d5679038ef9490
AbdirahmanOmar-90~ <cukashaomar92@gmail.com>
2026-09-02 22:16:00 +0300

    Add Exercise 32

--- Exercise_31/main.js
+++ Exercise_31/main.js
@@ -1,12 +1,12 @@
 async function getData() {
   try {
-    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    const response = await fetch(`https://api.github.com/users/mchamoudadev`)
     if (!response.ok) {
       throw new Error(`http error status,${response.status}`)
     }
     console.log('rospnse before json', response)
     const data = await response.json()
-    console.log('response after json', data)
+    console.log('response after json', data.public_repos)
   } catch (error) {}
 }
 getData()
