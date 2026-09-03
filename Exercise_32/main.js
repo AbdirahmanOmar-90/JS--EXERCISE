11827492f0a038ea295be7d3787c0c425594a9ac
AbdirahmanOmar-90~ <cukashaomar92@gmail.com>
2026-09-03 21:06:41 +0300

    last cosole

--- Exercise_32/main.js
+++ Exercise_32/main.js
@@ -1,2 +1,8 @@
 let h1 = document.querySelector('h1')
 console.log(h1)
+
+let p = document.querySelector('p')
+console.log(p)
+
+let button = document.querySelectorAll('button')
+console.log(button)
